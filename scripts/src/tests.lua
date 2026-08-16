e12cf8873699762aa08e698e3ec65f7717158783
Heath <45438677+simzy39@users.noreply.github.com>
2026-08-17 01:45:42 +1000

    tests: remove obsolete RGB SIMD source references

--- scripts/src/tests.lua
+++ scripts/src/tests.lua
@@ -55,13 +55,6 @@ project("mametests")
 	}
 
 	files {
-		MAME_DIR .. "src/emu/video/rgbsse.cpp",
-		MAME_DIR .. "src/emu/video/rgbsse.h",
-		MAME_DIR .. "src/emu/video/rgbvmx.cpp",
-		MAME_DIR .. "src/emu/video/rgbvmx.h",
-	}
-
-	files {
 		MAME_DIR .. "tests/main.cpp",
 		MAME_DIR .. "tests/lib/util/corestr.cpp",
 		MAME_DIR .. "tests/lib/util/cdrom.cpp",
