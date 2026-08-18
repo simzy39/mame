28e63f29530ede976fb5f7242184138d9c6c86a8
Heath <45438677+simzy39@users.noreply.github.com>
2026-08-18 15:18:32 +1000

    chd: Test index reconstruction with stored pregap

--- tests/lib/util/chd.cpp
+++ tests/lib/util/chd.cpp
@@ -165,3 +165,107 @@ TEST_CASE("CD CHD reconstructs track indexes from stored Q", "[util][chd][cdrom]
 	chd.close();
 	std::filesystem::remove_all(tempdir);
 }
+
+TEST_CASE("CD CHD reconstructs indexes relative to stored pregap", "[util][chd][cdrom]")
+{
+	const std::filesystem::path tempdir =
+			std::filesystem::temp_directory_path() / "mame-chd-cd-q-pregap-test";
+	const std::filesystem::path chdpath = tempdir / "pregap.chd";
+
+	std::filesystem::remove_all(tempdir);
+	std::filesystem::create_directories(tempdir);
+
+	constexpr uint32_t frame_count = 8;
+	constexpr uint32_t pregap = 2;
+	constexpr uint32_t hunk_bytes =
+			cdrom_file::FRAME_SIZE * cdrom_file::FRAMES_PER_HUNK;
+
+	const chd_codec_type compression[4] =
+	{
+		CHD_CODEC_NONE,
+		CHD_CODEC_NONE,
+		CHD_CODEC_NONE,
+		CHD_CODEC_NONE
+	};
+
+	chd_file chd;
+
+	REQUIRE_FALSE(chd.create(
+			chdpath.string(),
+			uint64_t(frame_count) * cdrom_file::FRAME_SIZE,
+			hunk_bytes,
+			cdrom_file::FRAME_SIZE,
+			compression));
+
+	cdrom_file::toc toc{};
+	toc.numtrks = 1;
+	toc.numsessions = 1;
+
+	cdrom_file::track_info &track = toc.tracks[0];
+	track.trktype = cdrom_file::CD_TRACK_AUDIO;
+	track.subtype = cdrom_file::CD_SUB_RAW;
+	track.datasize = cdrom_file::MAX_SECTOR_DATA;
+	track.subsize = cdrom_file::MAX_SUBCODE_DATA;
+	track.frames = frame_count;
+	track.pregap = pregap;
+	track.postgap = 0;
+
+	// Non-zero pgdatasize means the pregap sectors are physically stored.
+	track.pgtype = cdrom_file::CD_TRACK_AUDIO;
+	track.pgsub = cdrom_file::CD_SUB_RAW;
+	track.pgdatasize = cdrom_file::MAX_SECTOR_DATA;
+	track.pgsubsize = cdrom_file::MAX_SUBCODE_DATA;
+
+	track.control_flags = 0;
+	track.session = 0;
+
+	std::fill(std::begin(track.idx), std::end(track.idx), -1);
+
+	REQUIRE_FALSE(cdrom_file::write_metadata(&chd, toc));
+
+	std::vector<uint8_t> frame(cdrom_file::FRAME_SIZE, 0);
+
+	auto write_frame =
+			[&chd, &frame](
+					uint32_t framenum,
+					uint8_t index,
+					uint32_t relative_frame)
+			{
+				std::fill(frame.begin(), frame.end(), 0);
+
+				make_position_q(
+						index,
+						relative_frame,
+						150 + framenum - 2,
+						frame.data() + cdrom_file::MAX_SECTOR_DATA);
+
+				REQUIRE_FALSE(chd.write_units(framenum, frame.data()));
+			};
+
+	// Two physically stored pregap sectors.
+	write_frame(0, 0, 2);
+	write_frame(1, 0, 1);
+
+	// INDEX 01 begins at physical frame 2.
+	write_frame(2, 1, 0);
+	write_frame(3, 1, 1);
+	write_frame(4, 1, 2);
+
+	// INDEX 02 begins three frames after INDEX 01.
+	write_frame(5, 2, 3);
+	write_frame(6, 2, 4);
+	write_frame(7, 2, 5);
+
+	cdrom_file cd(&chd);
+
+	REQUIRE(cd.get_toc().tracks[0].idx[2] == -1);
+
+	cd.reconstruct_track_indexes();
+
+	// The physical transition occurs at frame 5, but idx[] coordinates are
+	// relative to INDEX 01 at physical frame 2.
+	REQUIRE(cd.get_toc().tracks[0].idx[2] == 3);
+
+	chd.close();
+	std::filesystem::remove_all(tempdir);
+}
