ac95ac5906e0912b42bd877d411f720242e9a978
Heath <45438677+simzy39@users.noreply.github.com>
2026-08-21 00:30:13 +0000

    chd: Validate compressed hunk lengths

--- src/lib/util/chd.cpp
+++ src/lib/util/chd.cpp
@@ -936,10 +936,6 @@ std::error_condition chd_file::codec_process_hunk(uint32_t hunknum)
 
 				// compressed case
 				uint8_t const *const rawmap = &m_rawmap[m_mapentrybytes * hunknum];
-				uint32_t const blocklen = get_u24be(&rawmap[1]);
-				if (UNEXPECTED(blocklen > m_compressed.size()))
-					return std::error_condition(error::INVALID_DATA);
-
 				uint64_t const blockoffs = get_u48be(&rawmap[4]);
 				switch (rawmap[0])
 				{
@@ -947,8 +943,12 @@ std::error_condition chd_file::codec_process_hunk(uint32_t hunknum)
 				case COMPRESSION_TYPE_1:
 				case COMPRESSION_TYPE_2:
 				case COMPRESSION_TYPE_3:
-					{
-						std::error_condition err = file_read(blockoffs, m_compressed.data(), blocklen);
+				{
+					uint32_t const blocklen = get_u24be(&rawmap[1]);
+					if (UNEXPECTED(blocklen > m_compressed.size()))
+						return std::error_condition(error::INVALID_DATA);
+
+					std::error_condition err = file_read(blockoffs, m_compressed.data(), blocklen);
 						if (UNEXPECTED(err))
 							return err;
 						auto &decompressor = *m_decompressor[rawmap[0]];
@@ -1098,10 +1098,6 @@ std::error_condition chd_file::read_hunk(uint32_t hunknum, void *buffer)
 				else
 				{
 					// compressed case
-					uint32_t const blocklen = get_u24be(&rawmap[1]);
-					if (UNEXPECTED(blocklen > m_compressed.size()))
-						return std::error_condition(error::INVALID_DATA);
-
 					uint64_t const blockoffs = get_u48be(&rawmap[4]);
 					util::crc16_t const blockcrc = get_u16be(&rawmap[10]);
 					switch (rawmap[0])
@@ -1111,6 +1107,10 @@ std::error_condition chd_file::read_hunk(uint32_t hunknum, void *buffer)
 					case COMPRESSION_TYPE_2:
 					case COMPRESSION_TYPE_3:
 						{
+							uint32_t const blocklen = get_u24be(&rawmap[1]);
+							if (UNEXPECTED(blocklen > m_compressed.size()))
+								return std::error_condition(error::INVALID_DATA);
+
 							std::error_condition err = file_read(blockoffs, m_compressed.data(), blocklen);
 							if (UNEXPECTED(err))
 								return err;
